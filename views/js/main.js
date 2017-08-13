cd0bdccd5d8b4025566ea2c87414b8cc3daa955d
aliallam <me.aliallam@gmail.com>
2017-08-13 14:10:41 +0200

    randomPizzaContainer

--- views/js/main.js
+++ views/js/main.js
@@ -450,8 +450,8 @@ var resizePizzas = function(size) {
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     var randomPizzaContainer = document.getElementsByClassName("randomPizzaContainer");
-
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    
+    for (var i = 0; i < randomPizzaContainer.length; i++) {
       var dx = determineDx(randomPizzaContainer[i], size);
       var newwidth = (randomPizzaContainer[i].offsetWidth + dx) + 'px';
       randomPizzaContainer[i].style.width = newwidth;
